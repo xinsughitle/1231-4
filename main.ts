69c7399f3dc6d82dad7e834f6276f49a0da14eac
xinsughitle <116938976+xinsughitle@users.noreply.github.com>
2022-12-31 14:48:36 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        BitRacer.motorRun(BitRacer.Motors.M_R, 300)
+        BitRacer.motorRun(BitRacer.Motors.M_L, 300)
+    }
+    if (receivedNumber == 0) {
+        BitRacer.motorRun(BitRacer.Motors.M_R, 0)
+        BitRacer.motorRun(BitRacer.Motors.M_L, 0)
+    }
+    if (receivedNumber == 2) {
+        BitRacer.motorRun(BitRacer.Motors.M_R, -300)
+        BitRacer.motorRun(BitRacer.Motors.M_L, -300)
+    }
+    if (receivedNumber == 3) {
+        BitRacer.motorRun(BitRacer.Motors.M_R, 300)
+        BitRacer.motorRun(BitRacer.Motors.M_L, -300)
+    }
+    if (receivedNumber == 4) {
+        BitRacer.motorRun(BitRacer.Motors.M_R, -300)
+        BitRacer.motorRun(BitRacer.Motors.M_L, 300)
+    }
 })
+radio.setGroup(1)
+radio.setTransmitPower(7)
