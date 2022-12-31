99fd2757029c3fe74d16c524c098d9562e023633
xinsughitle <116938976+xinsughitle@users.noreply.github.com>
2022-12-31 15:00:32 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        BitRacer.motorRun(BitRacer.Motors.M_R, 300)
-        BitRacer.motorRun(BitRacer.Motors.M_L, 300)
-    }
     if (receivedNumber == 0) {
-        BitRacer.motorRun(BitRacer.Motors.M_R, 0)
-        BitRacer.motorRun(BitRacer.Motors.M_L, 0)
+        BitRacer.motorRun(BitRacer.Motors.All, 0)
+    }
+    if (receivedNumber == 1) {
+        BitRacer.motorRun(BitRacer.Motors.All, 300)
     }
     if (receivedNumber == 2) {
-        BitRacer.motorRun(BitRacer.Motors.M_R, -300)
-        BitRacer.motorRun(BitRacer.Motors.M_L, -300)
+        BitRacer.motorRun(BitRacer.Motors.All, -300)
     }
     if (receivedNumber == 3) {
         BitRacer.motorRun(BitRacer.Motors.M_R, 300)
@@ -20,5 +17,4 @@ radio.onReceivedNumber(function (receivedNumber) {
         BitRacer.motorRun(BitRacer.Motors.M_L, 300)
     }
 })
-radio.setGroup(1)
-radio.setTransmitPower(7)
+radio.setGroup(255)
